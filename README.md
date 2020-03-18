1543a0ce8b6d99995c2ada9dfc87e3154f41383e
JM [DELL] <jmanuel.rar@emeraldigital.com>
2020-03-18 14:43:35 -0600

    Add knex to create databases and create routes file

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ Based in this article: [Building a RESTful API with Koa and Postgres](https://mh
 yarn test
 yarn start
 ```
+
+## Create databases in postgres
+
+```psql
+docker exec -i eclipse-postgres psql -U postgres -W
+
+# CREATE DATABASE koa_api;
+CREATE DATABASE
+# CREATE DATABASE koa_api_test;
+CREATE DATABASE
+```
--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,11 @@
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const Koa = require('koa');
+const indexRoutes = require('./routes/index');
 
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = {
-    status: 'success',
-    message: 'hello, world!'
-  };
-});
+app.use(indexRoutes.routes());
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
